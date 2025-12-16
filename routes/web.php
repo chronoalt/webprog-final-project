b5d6f7858edd541090fe59653e151c5442f62b35
Shardium <nitroshard@gmail.com>
2025-12-16 14:52:11 +0700

    Add temporary Inertia routes for navigation

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,16 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+#### Test Inertia routes
+# This is a shorthand. It's the same as Route::get(), but unable to run logic
+Route::inertia('/about', 'About')->name('about');
+
+# This is the full version of the above shorthand, which can include logic
+Route::get('/test', function () {return Inertia::render('test');}) ->name('test');
+
+Route::get('/login', function() {return Inertia::render('Auth/Login');})->name('login');
+Route::get('/register', function() {return Inertia::render('Auth/Register');})->name('register');
+Route::get('/inventory', function() {return Inertia::render('Inventory');})->name('inventory');
+####
+
 require __DIR__.'/settings.php';
